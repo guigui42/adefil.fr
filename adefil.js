92e14332704075ddda1cae32d71637ccd77b4761
guigui42 <guigui42@users.noreply.github.com>
2017-09-23 15:21:58 +0200

    cardholer

--- adefil.js
+++ adefil.js
@@ -12,18 +12,23 @@
  $.each(val, function (index, value) {
 
       var price = `<div class='col-lg-4 col-md-6 mb-4'>
+
               <div class='card h-100'>
                 <a href='#'><img class='card-img-top' src='https://placehold.it/700x400' alt=''></a>
+                <div class='card-footer'>
+                  <small class='text-muted'>`+this.vetement+`</small>
+                </div>
+                <div class='card-footer'>
+                  <small class='text-muted'>`+this.catgorie+`</small>
+                </div>
                 <div class='card-body'>
                   <h4 class='card-title'>
                     <a href='#'>`+this.action+`</a>
                   </h4>
                   <h5>`+this.prix+` €</h5>
-                  <p class='card-text'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</p>
+                  <p class='card-text'></p>
                 </div>
-                <div class='card-footer'>
-                  <small class='text-muted'>&#9733; &#9733; &#9733; &#9733; &#9734;</small>
-                </div>
+                
               </div>
             </div>`;
     $(price).appendTo("#priceslist");
