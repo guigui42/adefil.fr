ad4ca57eb2afcbe78a59b4c523bbfba9e30b8d76
guigui42 <guigui42@users.noreply.github.com>
2017-09-24 20:14:11 +0200

    removed old tab

--- adefil.js
+++ adefil.js
@@ -31,26 +31,7 @@ String.prototype.cleanup = function() {
 
  $.each(val, function (index, value) {
 
-      var price = `<div class='col-lg-4 col-md-6 mb-4 hidden prices `+key.cleanup()+`'  id="`+key.cleanup()+`"  >
 
-              <div class='card h-100'>
-               
-                <div class='card-footer'>
-                  <small class='text-muted'>`+this.vetement+`</small>
-                </div>
-                <div class='card-footer'>
-                  <small class='text-muted'>`+this.catgorie+`</small>
-                </div>
-                <div class='card-body'>
-                  <h4 class='card-title'>
-                    <a href='#'>`+this.action+`</a>
-                  </h4>
-                  <h5>`+this.prix+` €</h5>
-                  <p class='card-text'></p>
-                </div>
-                
-              </div>
-            </div>`;
 
   var price = `
      <tr class="prices hidden `+key.cleanup()+` id="`+key.cleanup()+`">
