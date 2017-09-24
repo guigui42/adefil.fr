7b42910441888ecb02107ec2fc1a07947c7cbad3
guigui42 <guigui42@users.noreply.github.com>
2017-09-24 18:44:55 +0200

    new menu

--- adefil.js
+++ adefil.js
@@ -1,17 +1,37 @@
+ function toggler(item, item2) {
+
+$(".prices").addClass('hidden');
+$(".menu").removeClass('active');
+//alert("dfd");
+ $("."+item).removeClass('hidden');
+  $("#menu"+item).addClass('active');
+ //$(item).toggle();
+ $("#title").text(  item2);
+}
+
   jQuery(document).ready(function($) {
+
+
+String.prototype.cleanup = function() {
+   return this.toLowerCase().replace(/[^a-zA-Z0-9]+/g, "");
+}
+
 
      $.getJSON( "prices.json", function( data ) {
 
 
+
+
+
   var items = [];
   $.each( data, function( key, val ) {
-    var link = "<a href='#"+key+"' class='list-group-item'>"+key+"</a>";
+    var link = "<a href='#"+key.cleanup()+"' onClick=\"toggler('"+key.cleanup()+"', '"+key+"');\" class='list-group-item menu' id='menu"+key.cleanup()+"'>"+key+"</a>";
     $( link).appendTo( "#list1" );
 
 
  $.each(val, function (index, value) {
 
-      var price = `<div class='col-lg-4 col-md-6 mb-4'>
+      var price = `<div class='col-lg-4 col-md-6 mb-4 hidden prices `+key.cleanup()+`'  id="`+key.cleanup()+`"  >
 
               <div class='card h-100'>
                 <a href='#'><img class='card-img-top' src='https://placehold.it/700x400' alt=''></a>
