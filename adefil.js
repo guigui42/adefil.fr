f598b1c44fff25da9fb320bbbb9c73e42df88bf5
guigui42 <guigui42@users.noreply.github.com>
2017-11-17 10:00:20 +0100

    top button

--- adefil.js
+++ adefil.js
@@ -69,7 +69,7 @@ function scrollFunction() {
 }
 
 // When the user clicks on the button, scroll to the top of the document
-function topFunction() {
+function topFunction2() {
     document.body.scrollTop = 0; // For Chrome, Safari and Opera 
     document.documentElement.scrollTop = 0; // For IE and Firefox
 }
